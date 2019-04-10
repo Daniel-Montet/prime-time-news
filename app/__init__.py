8808f0f550ce84d056b77e5ad4081ddceb90bb28
Daniel Montet <dmontetproff@gmail.com>
2019-04-10 16:06:50 +0300

    Add bootstrap and navbar template

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 
 """
 initialize application
+
 """
 app= Flask(__name__,instance_relative_config= True)
+
+# Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
 
 from app import views
